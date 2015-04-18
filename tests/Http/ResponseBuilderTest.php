ad2c2f45b09095e3b98873785412ff542a3643c8
Jack Wall <jw@jack.gd>
2015-04-18 18:11:52 +0100

    Custom adding Meta and Custom Status and Code Headers

---
+++ tests/Http/ResponseBuilderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Mockery as m;
+use Illuminate\Cookie\CookieJar as Cookie;
+use j4k\Api\Http\ResponseBuilder;
+
+class ResponseBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testAddingMeta()
+    {
+        $builder = new ResponseBuilder([]);
+        $builder->meta('foo', 'bar');
+
+        $response = $builder->build();
+
+        $this->assertEquals('{"meta":{"foo":"bar"}}', $response->getContent());
+    }
+
+    public function testBuildingWithCustomStatusCodeAndHeaders()
+    {
+        $builder = new ResponseBuilder(['key' => 'value']);
+
+        $builder->setStatusCode(404);
+        $builder->withHeader('Header', 'value');
+
+        $response = $builder->build();
+
+        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertEquals('value', $response->headers->get('Header'));
+    }
+
+}
